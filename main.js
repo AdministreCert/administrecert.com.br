1dc83018aa0e4a5819133c3e019dddf8952df35e
Celio Rodrigues <celio-rodrigues21@hotmail.com>
2025-12-13 19:52:38 -0300

    Links e imagens

---
+++ main.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', () => {
+  const features = document.querySelector('.features');
+
+  const onClick = e => {
+    var href = e.target.getAttribute('href');
+    if (href && href.length > 1 && href.indexOf('#') === 0) {
+      e.preventDefault();
+      var section = document.querySelector(href);
+      if (section) {
+        window.scrollTo({ top: section.offsetTop, behavior: 'smooth' });
+      }
+    }
+  };
+
+  document.querySelectorAll('a').forEach(it => it.addEventListener('click', onClick, false));
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= 480) {
+      if (!features.classList.contains('animated')) {
+        features.classList.add('animated');
+      }
+    } else if (window.scrollY <= 50) {
+      features.classList.remove('animated');
+    }
+  }, false);
+}, false);
